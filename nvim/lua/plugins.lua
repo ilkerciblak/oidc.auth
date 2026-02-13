b363e6172f00a17c4b9b3b5c10dcb9136589f074
ilkerciblak <ilkerciblak@gmail.com>
2026-02-13 09:13:47 +0300

    ops(nvim.config): add hidden file functionality to telescope plugin

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,21 +1,34 @@
 return {
-	{
-		"nvim-neo-tree/neo-tree.nvim",
-		branch = "v3.x",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"MunifTanjim/nui.nvim",
-			"nvim-tree/nvim-web-devicons", -- optional, but recommended
-		},
-		lazy = false, -- neo-tree will lazily load itself
-		config = function()
-			require("neo-tree").setup({
-				window = {
-					position = "right",
-					width = 40,
-				},
-			})
-			vim.keymap.set("n", "<C-n>", ":Neotree filesystem toggle position=right<CR>", {})
-		end,
-	},
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-web-devicons", -- optional, but recommended
+    },
+    lazy = false,                 -- neo-tree will lazily load itself
+    config = function()
+      require("neo-tree").setup({
+        close_if_last_window = true,
+        window = {
+          position = "right",
+          width = 40,
+        },
+        filesystem = {
+          filtered_items = {
+            hide_by_name = {
+              "node_modules",
+              ".git",
+            },
+            hide_dotfiles = false,
+            always_show_by_pattern = {
+              ".env*"
+            }
+          },
+        },
+      })
+      vim.keymap.set("n", "<C-n>", ":Neotree filesystem toggle position=right<CR>", {})
+    end,
+  },
 }

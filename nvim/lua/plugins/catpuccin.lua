b2bde907afa924cdcef7973cf2a3a6124ed17667
ilkerciblak <ilkerciblak@gmail.com>
2026-02-19 10:18:35 +0300

    chore(dev-env): edit development environment theme

--- nvim/lua/plugins/catpuccin.lua
+++ nvim/lua/plugins/catpuccin.lua
@@ -1,7 +1,19 @@
 return {
-  "catppuccin/nvim", name = "catppuccin", priority = 1000,
-  config = function()
-    require("catppuccin").setup()
-    vim.cmd.colorscheme  "catppuccin-mocha"
-  end
+	{
+			"catppuccin/nvim",
+			name = "catppuccin",
+			priority = 1000,
+			config = function()
+				require("catppuccin").setup({
+
+        lsp_styles ={
+          inlay_hints = {
+            background= true,
+          },
+        },
+        auto_intergrations = true,
+        })
+				vim.cmd.colorscheme("catppuccin-mocha")
+			end,
+		},
 }
